1fc4b8f89bd03773c3f0b8acbe29d148aa79b09a
spyh <athanokwibisono@gmail.com>
2025-11-25 09:04:07 +0700

    fix: symlink time_sync

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -2,8 +2,8 @@
 
 python3 peer_node.py \
 	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
+	  --peers A@192.168.122.110:5000 B@192.168.122.120:5001 D@192.168.122.140:5002 \
+	  --logger 192.168.122.130 9999 \
 	  --offset-ms 600 \
 	  --initiate-broadcast --msg "Hello from A"
 
--- synchronization/time_sync/node_b/run.bash
+++ synchronization/time_sync/node_b/run.bash
@@ -2,7 +2,7 @@
 
 python3 peer_node.py \
 	  --name B --listen 0.0.0.0 5001 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
+	  --peers A@192.168.122.110:5000 B@192.168.122.120:5001 D@192.168.122.140:5002 \
+          --logger 192.168.122.130 9999 \
 	  --offset-ms -600 
 
