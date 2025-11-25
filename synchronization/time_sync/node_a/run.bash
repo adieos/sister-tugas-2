6968c5e8e950cfbfce0dfeeb720a20214ca5ab31
spyh <athanokwibisono@gmail.com>
2025-11-25 09:15:36 +0700

    fix: remove newline shenanigans

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@192.168.122.110:5000 B@192.168.122.120:5001 D@192.168.122.140:5002 \
-	  --logger 192.168.122.130 9999 \
-	  --offset-ms 600 \
-	  --initiate-broadcast --msg "Hello from A"
+python3 peer_node.py --name A --listen 0.0.0.0 5000 --peers A@192.168.122.110:5000 B@192.168.122.120:5001 D@192.168.122.140:5002 --logger 192.168.122.130 9999 --offset-ms 600 --initiate-broadcast --msg "Hello from A"
 
